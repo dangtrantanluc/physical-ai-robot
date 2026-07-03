5e335ba40fdc0287024eb35baaa221432e9c7a3c
dangtrantanluc <dttanluc2004@gmail.com>
2026-07-03 14:07:30 +0700

    fix

---
+++ frontend/android/app/.cxx/Debug/1f683v1r/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-vision-camera/react-native-vision-cameraConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-vision-camera::VisionCamera)
+add_library(react-native-vision-camera::VisionCamera SHARED IMPORTED)
+set_target_properties(react-native-vision-camera::VisionCamera PROPERTIES
+    IMPORTED_LOCATION "/home/tanluc/workspace/physical-ai/frontend/node_modules/react-native-vision-camera/android/build/intermediates/cxx/Debug/4w4p2e54/obj/x86_64/libVisionCamera.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/tanluc/workspace/physical-ai/frontend/node_modules/react-native-vision-camera/android/build/headers/visioncamera"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
